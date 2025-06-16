860ab654a6dfc98b0d4c6f1f12ebcf79a9552dcc
Hafidz Izzudin <hafidz.izzudin49@gmail.com>
2025-06-16 11:14:54 +0700

    feat: add db connection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from sqlalchemy import Engine
+from sqlmodel import create_engine
 
+from infra import settings
 from repository.item import ItemRepository
 from routes.init import Router
 from routes.item import ItemRouter
@@ -12,6 +15,14 @@ def init_router(app: FastAPI) -> Router:
     return Router(app, [item_router])
 
 
+def init_db() -> Engine:
+    sqlite_url = f"sqlite:///{settings.settings.db_path}"
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(sqlite_url, connect_args=connect_args)
+    return engine
+
+
+db_engine = init_db()
 app = FastAPI()
 router = init_router(app)
 router.init()
